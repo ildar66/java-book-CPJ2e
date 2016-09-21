29c9521b28299e45f9008e6009474b32df50d3e0
ildar shafigullin <ildar66@inbox.ru>
2016-09-21 15:42:59 +0300

    Dummy standin for referenced generic "Helper" classes

---
+++ src/chapter_0/Helper.java
@@ -0,0 +1,15 @@
+package chapter_0;
+
+/**
+ * @author Doug Lea
+ */
+public class Helper {
+
+    // Dummy standin for referenced generic "Helper" classes
+    public void handle() {
+    }
+
+    public void operation() {
+    }
+
+}

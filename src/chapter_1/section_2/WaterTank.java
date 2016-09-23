30f5b08e9fe17ac3e566f0fc4e869d063e72cd03
ildar shafigullin <ildar66@inbox.ru>
2016-09-23 11:54:47 +0300

    add comment for WaterTank

--- src/chapter_1/section_2/WaterTank.java
+++ src/chapter_1/section_2/WaterTank.java
@@ -8,8 +8,8 @@ import chapter_0.UnderflowException;
  */
 class WaterTank {
 
-    final float capacity;
-    float currentVolume = 0.0f;
+    final float capacity; //Attributes
+    float currentVolume = 0.0f; //Invariant (between zero and capacity)
     WaterTank overflow;
 
     WaterTank(float cap) {
@@ -17,7 +17,21 @@ class WaterTank {
         //...
     }
 
-    void addWater(float amount) throws OverflowException {}
+    //Operations:
 
-    void removeWater(float amount) throws UnderflowException {}
+    //Preconditions and postconditions:
+    //on the effects of operations, such as rules stating that it is
+    //impossible to remove water from an empty tank, or to add water to a full tank that is not
+    //equipped with an available overflow tank.
+
+    //Protocols:
+    //constraining when and how messages (operation requests) are processed. For
+    //example, we may impose a rule that at most one addWater or removeWater message
+    //is processed at any given time or, alternatively, a rule stating that removeWater messages
+    //are allowed in the midst of addWater operations.
+    void addWater(float amount) throws OverflowException {
+    }
+
+    void removeWater(float amount) throws UnderflowException {
+    }
 }

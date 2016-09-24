da97f5990d13340d559aa501e1f103413d26a1ee
ildar shafigullin <ildar66@inbox.ru>
2016-09-24 10:46:22 +0300

    add Design Forces section

---
+++ src/chapter_1/section_3/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 24.09.2016.
+ * Design Forces
+ *
+ * Safety: Nothing bad ever happens to an object. Safety failures lead to unintended behavior at run time — things just start going wrong.
+ * Liveness: Something eventually happens within an activity.Liveness failures lead to no behavior — things just stop running.
+ * Reusability: The utility of objects and classes across multiple contexts.
+ * Performance: The extent to which activities execute soon and quickly.
+ */
+package chapter_1.section_3;

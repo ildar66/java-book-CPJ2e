48fb4488128f3ccbb071776866093af68a7464fd
ildar shafigullin <ildar66@inbox.ru>
2016-09-25 12:00:13 +0300

    Adapters can be used to provide before/after control(wrapping pattern)

---
+++ src/chapter_1/section_4/AdaptedTank.java
@@ -0,0 +1,48 @@
+package chapter_1.section_4;
+
+import chapter_0.OverflowException;
+import chapter_0.UnderflowException;
+
+// Adapters can be used to provide before/after control merely by wrapping the delegated call within the control actions.
+class AdaptedTank implements Tank {
+
+    protected final Tank delegate;
+
+    public AdaptedTank(Tank t) {
+        delegate = t;
+    }
+
+    @Override
+    public float getCapacity() {
+        return delegate.getCapacity();
+    }
+
+    @Override
+    public float getVolume() {
+        return delegate.getVolume();
+    }
+
+    protected void checkVolumeInvariant() throws java.lang.AssertionError {
+        float v = getVolume();
+        float c = getCapacity();
+        if (!(v >= 0.0 && v <= c)) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public void transferWater(float amount) throws OverflowException, UnderflowException {
+        checkVolumeInvariant();
+
+        try {
+            delegate.transferWater(amount);
+        } catch (OverflowException ex) {
+            throw ex;
+        } catch (UnderflowException ex) {
+            throw ex;
+        } finally {
+            checkVolumeInvariant();
+        }
+    }
+
+}

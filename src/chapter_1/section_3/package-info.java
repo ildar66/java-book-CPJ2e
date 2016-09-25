3564fd165c074c7810ce81f67b430d8ded7f89ab
ildar shafigullin <ildar66@inbox.ru>
2016-09-25 11:12:24 +0300

    add Before/After Patterns section

--- src/chapter_1/section_3/package-info.java
+++ src/chapter_1/section_3/package-info.java
@@ -1,9 +1,8 @@
 /**
  * Created by User on 24.09.2016.
  * Design Forces
- *
  * Safety: Nothing bad ever happens to an object. Safety failures lead to unintended behavior at run time — things just start going wrong.
- * Liveness: Something eventually happens within an activity.Liveness failures lead to no behavior — things just stop running.
+ * Liveness: Something eventually happens within an activity. Liveness failures lead to no behavior — things just stop running.
  * Reusability: The utility of objects and classes across multiple contexts.
  * Performance: The extent to which activities execute soon and quickly.
  */
---
+++ src/section_4/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 25.09.2016.
+ * Before/After Patterns
+ */
+package section_4;

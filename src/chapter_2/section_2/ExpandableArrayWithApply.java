3fcfabd72d21a2009683208d4c29c7d1235c856c
ildar shafigullin <ildar66@inbox.ru>
2016-09-27 10:20:38 +0300

    Synchronized aggregate operations sample

---
+++ src/chapter_2/section_2/ExpandableArrayWithApply.java
@@ -0,0 +1,25 @@
+package chapter_2.section_2;
+
+/*
+* Synchronized aggregate operations
+    While this is often acceptable, it may lead to the kinds of liveness and performance problems
+    that motivated the default rule, saying to release locks when making method calls (here, to apply).
+*/
+class ExpandableArrayWithApply extends ExpandableArray {
+
+    public ExpandableArrayWithApply(int cap) {
+        super(cap);
+    }
+
+    synchronized void applyToAll(Procedure p) {
+        for (int i = 0; i < size; ++i)
+            p.apply(data[i]);
+    }
+    /**    USAGE:
+     v.applyToAll(new Procedure() {
+        public void apply(Object obj) {
+            System.out.println(obj);
+        }
+     } );
+     */
+}
---
+++ src/chapter_2/section_2/Procedure.java
@@ -0,0 +1,7 @@
+package chapter_2.section_2;
+
+//used in example Synchronized aggregate operations
+interface Procedure {
+
+    void apply(Object obj);
+}

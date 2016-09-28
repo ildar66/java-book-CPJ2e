90505d80dd5381c29c4eb2d61eaca4704d3b82f8
ildar shafigullin <ildar66@inbox.ru>
2016-09-28 11:24:41 +0300

    add comment on Volatile para

--- src/chapter_2/section_2/VFloat.java
+++ src/chapter_2/section_2/VFloat.java
@@ -4,7 +4,6 @@ package chapter_2.section_2;
    Volatile para:
    In terms of atomicity, visibility, and ordering, declaring a field as volatile is nearly identical in
    effect to using a little fully synchronized class protecting only that field via get/set methods, as in:
-   Declaring a field as volatile differs only in that no locking is involved.
 */
 final class VFloat {
 
@@ -18,5 +17,8 @@ final class VFloat {
     final synchronized void set(float f) {
         this.value = f;
     }
+
+    /*Using volatile fields can make sense when it is somehow known that only one thread can change
+    a field, but many other threads are allowed to read it at any time.*/
 
 }

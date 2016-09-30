1ee94ca07563e2b8568e39c2605e74d4b8214004
ildar shafigullin <ildar66@inbox.ru>
2016-09-30 11:07:54 +0300

    add Structuring and Refactoring Classes section

---
+++ src/chapter_2/section_4/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by User on 30.09.2016.
+ *
+ * Structuring and Refactoring Classes section
+ *
+ * This section describes strategies for removing unnecessary synchronization,
+ * splitting synchronization to match functionality, exporting read-only operations via
+ * adapters, isolating state representations to reduce access costs or improve potential parallelism, and
+ * grouping objects to use common locks so as to mirror layered designs.
+ */
+package chapter_2.section_4;

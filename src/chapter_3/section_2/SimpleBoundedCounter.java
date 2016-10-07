2f9f491d860968d48865738432215ef83341e19d
ildar shafigullin <ildar66@inbox.ru>
2016-10-07 11:22:10 +0300

    Notifications para

---
+++ src/chapter_3/section_2/SimpleBoundedCounter.java
@@ -0,0 +1,49 @@
+package chapter_3.section_2;
+
+/*
+// Notifications
+*
+The simplest way to arrange that blocked threads eventually recheck conditions is to insert
+  notifyAll in methods that cause relevant state changes. In turn, the simplest way to do this is to
+  define utility methods that encapsulate assignment, issuing a notification upon any change in value.
+For example,here is a first pass at BoundedCounter:
+*/
+
+public class SimpleBoundedCounter {
+
+    static final long MIN = 0; // minimum allowed value
+
+    static final long MAX = 10; // maximum allowed value
+
+    protected long count = MIN;
+
+    public synchronized long count() {
+        return count;
+    }
+
+    public synchronized void inc() throws InterruptedException {
+        awaitUnderMax();
+        setCount(count + 1);
+    }
+
+    public synchronized void dec() throws InterruptedException {
+        awaitOverMin();
+        setCount(count - 1);
+    }
+
+    protected void setCount(long newValue) { // PRE: lock held
+        count = newValue;
+        notifyAll(); // wake up any thread depending on new value
+    }
+
+    protected void awaitUnderMax() throws InterruptedException {
+        while (count == MAX)
+            wait();
+    }
+
+    protected void awaitOverMin() throws InterruptedException {
+        while (count == MIN)
+            wait();
+    }
+
+}

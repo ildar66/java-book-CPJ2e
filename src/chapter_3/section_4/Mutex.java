55fdb783695e1746e51b6263356fce56586bed01
ildar shafigullin <ildar66@inbox.ru>
2016-10-12 12:15:04 +0300

    Mutual exclusion locks add para

--- src/chapter_3/section_4/Mutex.java
+++ src/chapter_3/section_4/Mutex.java
@@ -6,6 +6,8 @@ import EDU.oswego.cs.dl.util.concurrent.Sync;
 /**
  * Created by User on 12.10.2016.
  * Mutual exclusion locks
+ * Semaphores can be used to implement mutual exclusion locks simply by initializing the number of
+ * permits to 1. For example, a Mutex class could be defined as:
  */
 class Mutex implements Sync {
 

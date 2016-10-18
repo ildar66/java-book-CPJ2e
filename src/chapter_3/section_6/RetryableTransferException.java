f42e2ce039cb0a97a3706d65fdf0c6e6d8ec21b4
ildar shafigullin <ildar66@inbox.ru>
2016-10-18 10:24:39 +0300

    Creating Transactions. Example.

---
+++ src/chapter_3/section_6/RetryableTransferException.java
@@ -0,0 +1,6 @@
+package chapter_3.section_6;
+
+// Creating Transactions. Example.
+class RetryableTransferException extends Exception {
+
+}
---
+++ src/chapter_3/section_6/TransactionLogger.java
@@ -0,0 +1,15 @@
+package chapter_3.section_6;
+
+// Creating Transactions. Example.
+class TransactionLogger {
+
+    void cancelLogEntry(Transaction t, long amount, TransBankAccount src, TransBankAccount dst) {
+    }
+
+    void logTransfer(Transaction t, long amount, TransBankAccount src, TransBankAccount dst) {
+    }
+
+    void logCompletedTransfer(Transaction t, long amount, TransBankAccount src, TransBankAccount dst) {
+    }
+
+}

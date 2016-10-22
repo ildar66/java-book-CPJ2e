1ae88fa08e94a765a54ce6eb5b20f621b98374ed
ildar shafigullin <ildar66@inbox.ru>
2016-10-22 13:19:08 +0300

    Composing Oneway Messages add Interfaces para

---
+++ src/chapter_4/section_2/DualInputPushStage.java
@@ -0,0 +1,7 @@
+package chapter_4.section_2;
+
+// Interfaces
+interface DualInputPushStage extends PushStage {
+
+    void putB(Box p);
+}
---
+++ src/chapter_4/section_2/PushStage.java
@@ -0,0 +1,8 @@
+package chapter_4.section_2;
+
+// Interfaces
+interface PushStage {
+
+    void putA(Box p);
+
+}

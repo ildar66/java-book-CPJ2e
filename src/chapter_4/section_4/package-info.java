66e4bea466004237c8e8732dd83b367801f227d8
ildar shafigullin <ildar66@inbox.ru>
2016-10-27 11:11:35 +0300

    Parallel Decomposition section. Fork/Join Fib

--- src/chapter_4/section_4/package-info.java
+++ src/chapter_4/section_4/package-info.java
@@ -1,5 +1,26 @@
-/**
+/*
  * Created by User on 26.10.2016.
  * Parallel Decomposition section.
+ * We focus on three families of task-based designs, fork/join parallelism, computation trees, and
+ * barriers:
+ *
+ *  Fork/Join
+ *  Fork/join decomposition relies on parallel versions of divide-and-conquer techniques familiar in
+    sequential algorithm design. Solutions take the form:
+        pseudoclass Solver { // Pseudocode
+            // ...
+            Result solve(Param problem) {
+                if (problem.size <= BASE_CASE_SIZE)
+                    return directlySolve(problem);
+                else {
+                    Result l, r;
+                    IN-PARALLEL {
+                        l = solve(lefthalf(problem));
+                        r = solve(rightHalf(problem));
+                    }
+                    return combine(l, r);
+                }
+            }
+        }
  */
 package chapter_4.section_4;
